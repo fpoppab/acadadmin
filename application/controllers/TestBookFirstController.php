57ae22c8c5a40e9cbdc749139d36e2b03100dc89
Chairattha <chairattha.facts@gmail.com>
2024-06-10 13:30:11 +0700

    [+] add Controller for test today

---
+++ application/controllers/TestBookFirstController.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class TestBookFirstController extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+}
