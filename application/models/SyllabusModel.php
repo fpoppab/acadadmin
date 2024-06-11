17ff81714a782c51bb6fef88b27aae0ca4b4c9d4
THIDARAT <artitbook43@gmail.com>
2024-06-11 14:45:06 +0700

    [~] remodel name [~] remove wrong column

--- application/models/SyllabusModel.php
+++ application/models/SyllabusModel.php
@@ -1,11 +1,11 @@
 <?php
-class SyllbusModel extends CI_Model
+class SyllabusModel extends CI_Model
 {
 
     #ดึงข้อมูลกลุ่มสาระ
     public function get_group_learning()
     {
-        $this->db->select('g.id as group_learning_id,g.name as group_learning_name,g.name as group_learning_name,g.year as group_learning_year');
+        $this->db->select('g.id as group_learning_id,g.name as group_learning_name,g.year as group_learning_year');
         $this->db->from('tb_group_learning g');
         $this->db->where(array('g.displaystatus' => 1));
         $query = $this->db->get();
