6c9514e71055181df142e6ffa58cf3406d8f29af
Chairattha <chairattha.facts@gmail.com>
2024-07-31 03:26:52 +0700

    [+] add datatable function and imagelogo function for reduce the number of lines

--- application/helpers/element_helper.php
+++ application/helpers/element_helper.php
@@ -1,5 +1,5 @@
 <?php
-function _label($text,$clss="")
+function _label($text, $clss = "")
 {
     echo "<div class=\"form-label {$clss}\">" . language($text) . "</div>";
 }
@@ -24,4 +24,65 @@ function language($text, $language = null)
         $result = $json_data[$language][$text];
     }
     return $result;
+}
+
+function datatable($config)
+{
+    if (!empty($config["tablename"])) {
+        $tb = "
+        <script>
+            $('#" . $config["tablename"] . "').DataTable({
+                responsive: true,
+                columnDefs: [
+                    {
+                        className: 'dtr-control arrow-right',
+                        orderable: false,  
+                    }
+                ],
+                language: {
+                    url: '" . base_url("languages/en_DataTable.json") . "'
+                }
+            });
+        </script>";
+        return $tb;
+    } else {
+        return "";
+    }
+
+}
+
+function imagelogo($config)
+{
+    if (!empty($config["inputID"])) {
+        $tb = "
+        <input type='file' id='" . $config["inputID"] . "' style='display:none;' />
+        <div class='col-auto'>
+            <span class='avatar avatar-xl btn' id='" . $config["inputID"] . "-logo-image'
+                style='background-image: url(\"" . base_url('resource/no-image.jpg') . "\")'>
+            </span>
+        </div>
+        <script>
+            $('#" . $config["inputID"] . "-logo-image').click(function () {
+                $('#" . $config["inputID"] . "').trigger('click');
+            });
+            $('#" . $config["inputID"] . "').change(function () {
+                if (this.files && this.files[0]) {
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        $('#" . $config["inputID"] . "-logo-image').css('background-image', 'url(' + e.target.result + ')');
+                    }
+                    reader.readAsDataURL(this.files[0]);
+                }
+            });
+        </script> ";
+        return $tb;
+    } else {
+        return "";
+    }
+
+
+
+
+
+
 }
