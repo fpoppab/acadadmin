a79ad93d9364e2a151bfd1263b30b9f7137e6005
Chairattha <chairattha.facts@gmail.com>
2024-08-03 03:09:40 +0700

    [+] add datepicker helper for new element

--- application/helpers/element_helper.php
+++ application/helpers/element_helper.php
@@ -79,10 +79,107 @@ function imagelogo($config)
     } else {
         return "";
     }
+}
+
+function _datepickerth($name, $id, $epoch = 0, $type = "student")
+{
+    if (!empty($epoch)) {
+        $date = date("Y-m-d", $epoch);
+    } else {
+        $date = date("Y-m-d");
+    }
+    $pieces = explode("-", $date);
+
+    $day = $pieces[2];
+    $dayArr = array(
+        "01" => 1,
+        "02" => 2,
+        "03" => 3,
+        "04" => 4,
+        "05" => 5,
+        "06" => 6,
+        "07" => 7,
+        "08" => 8,
+        "09" => 9,
+        "10" => 10,
+        "11" => 11,
+        "12" => 12,
+        "13" => 13,
+        "14" => 14,
+        "15" => 15,
+        "16" => 16,
+        "17" => 17,
+        "18" => 18,
+        "19" => 19,
+        "20" => 20,
+        "21" => 21,
+        "22" => 22,
+        "23" => 23,
+        "24" => 24,
+        "25" => 25,
+        "26" => 26,
+        "27" => 27,
+        "28" => 28,
+        "29" => 29,
+        "30" => 30,
+        "31" => 31,
+    );
+
+    $dp = "
+        <span style='width:30%;float: left;'>
+            <select id='" . $id . "Day' name='" . $id . "Day' class='form-select'>";
+    foreach ($dayArr as $keyD => $d) {
+        $select = ($keyD == $day) ? "selected" : "";
+        $dp .= "<option value='$keyD' $select>$d</option>";
+    }
+    $dp .= "</select>
+        </span>";
 
 
+    $month = $pieces[1];
+    $monthArr = array(
+        "01" => "ม.ค.",
+        "02" => "ก.พ.",
+        "03" => "มี.ค.",
+        "04" => "เม.ย.",
+        "05" => "พ.ค.",
+        "06" => "มิ.ย.",
+        "07" => "ก.ค.",
+        "08" => "ส.ค.",
+        "09" => "ก.ย.",
+        "10" => "ต.ค.",
+        "11" => "พ.ย.",
+        "12" => "ธ.ค.",
+    );
+    $dp .= "
+        <span style='width:35%;float: left;'>
+            <select id='" . $id . "Month' name='" . $id . "Month' class='form-select'>";
+    foreach ($monthArr as $keyM => $m) {
+        $select = ($keyM == $month) ? "selected" : "";
+        $dp .= "<option value='$keyM' $select>$m</option>";
+    }
+    $dp .= "
+            </select>
+        </span>";
 
+    $year = $pieces[0] + 543;
+    $dp .= "
+        <span style='width:35%;float: left;'>
+            <select id='" . $id . "Year' name='" . $id . "Year' class='form-select'>";
+    if ($type == "student") {
+        for ($y = 2567; $y >= 2547; $y -= 1) {
+            $select = ($y == $year) ? "selected" : "";
+            $dp .= "<option value='" . ($y - 543) . "' $select>$y</option>";
+        }
+    } else {
+        for ($y = 2567; $y >= 2497; $y -= 1) {
+            $select = ($y == $year) ? "selected" : "";
+            $dp .= "<option value='" . ($y - 543) . "' $select>$y</option>";
+        }
+    }
+    $dp .= "</select>
+        </span>
+    </select>";
 
-
-
+    return $dp;
 }
--- application/helpers/system_helper.php
+++ application/helpers/system_helper.php
@@ -1,6 +1,7 @@
 <?php
 
-function get_client_ip() {
+function get_client_ip()
+{
     $ipaddress = '';
     if (getenv('HTTP_CLIENT_IP'))
         $ipaddress = getenv('HTTP_CLIENT_IP');
@@ -19,71 +20,95 @@ function get_client_ip() {
     return $ipaddress;
 }
 
-function todate($epoch) {
+function todate($epoch)
+{
     return date("Y-m-d", $epoch);
 }
 
-function tothaidate($epoch) {
-    $newEpochTime = strtotime("+543 years", $epoch);
-    return date("j/n/", $epoch) . date("Y", $newEpochTime);
+// function tothaidate($epoch)
+// {
+//     $newEpochTime = strtotime("+543 years", $epoch);
+//     return date("j/n/", $epoch) . date("Y", $newEpochTime);
+// }
+
+function tothaishortdate($epoch)
+{
+    if (!empty($epoch)) {
+        $date = date("Y-m-d", substr($epoch, 0, 10));
+        $pieces = explode("-", $date);
+        $day = $pieces[2];
+        $month = $pieces[1];
+        $year = $pieces[0] + 543;
+        return $day . " " . tothaishortmonth($month) . " " . $year;
+    } else {
+        return "";
+    }
 }
 
-function tothaishortdate($epoch) {
-    $newEpochTime = strtotime("+543 years", $epoch);
-    return date("j ") . tothaishortmonth(date("n", $epoch)) . date(" y", $newEpochTime);
+function tothaifulldate($epoch)
+{
+    if (!empty($epoch)) {
+        $date = date("Y-m-d", substr($epoch, 0, 10));
+        $pieces = explode("-", $date);
+        $day = $pieces[2];
+        $month = $pieces[1];
+        $year = $pieces[0] + 543;
+        return $day . " " . tothaifullmonth($month) . " " . $year;
+    } else {
+        return "";
+    }
 }
 
-function tothaifulldate($epoch) {
-    $newEpochTime = strtotime("+543 years", $epoch);
-    return date("j ") . tothaifullmonth(date("n", $epoch)) . date(" Y", $newEpochTime);
-}
-
-function tothaifullmonth($num) {
+function tothaifullmonth($num)
+{
     if ($num >= 1 && $num <= 12) {
         $thaiMonth = array(
-            1 => "มกราคม",
-            2 => "กุมภาพันธ์",
-            3 => "มีนาคม",
-            4 => "เมษายน",
-            5 => "พฤษภาคม",
-            6 => "มิถุนายน",
-            7 => "กรกฎาคม",
-            8 => "สิงหาคม",
-            9 => "กันยายน",
-            10 => "ตุลาคม",
-            11 => "พฤศจิกายน",
-            12 => "ธันวาคม",
+            "01" => "มกราคม",
+            "02" => "กุมภาพันธ์",
+            "03" => "มีนาคม",
+            "04" => "เมษายน",
+            "05" => "พฤษภาคม",
+            "06" => "มิถุนายน",
+            "07" => "กรกฎาคม",
+            "08" => "สิงหาคม",
+            "09" => "กันยายน",
+            "10" => "ตุลาคม",
+            "11" => "พฤศจิกายน",
+            "12" => "ธันวาคม",
         );
         return $thaiMonth[$num];
     }
 }
 
-function tothaishortmonth($num) {
+function tothaishortmonth($num)
+{
     if ($num >= 1 && $num <= 12) {
         $thaiMonth = array(
-            1 => "ม.ค.",
-            2 => "ก.พ.",
-            3 => "มี.ค.",
-            4 => "เม.ย.",
-            5 => "พ.ค.",
-            6 => "มิ.ย.",
-            7 => "ก.ค.",
-            8 => "ส.ค.",
-            9 => "ก.ย.",
-            10 => "ต.ค.",
-            11 => "พ.ย.",
-            12 => "ธ.ค.",
+            "01" => "ม.ค.",
+            "02" => "ก.พ.",
+            "03" => "มี.ค.",
+            "04" => "เม.ย.",
+            "05" => "พ.ค.",
+            "06" => "มิ.ย.",
+            "07" => "ก.ค.",
+            "08" => "ส.ค.",
+            "09" => "ก.ย.",
+            "10" => "ต.ค.",
+            "11" => "พ.ย.",
+            "12" => "ธ.ค.",
         );
         return $thaiMonth[$num];
     }
 }
 
-function thaimoth_to_month_db($month) {
+function thaimoth_to_month_db($month)
+{
     $thaimonth = array("ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค.");
     return str_replace(array('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'), $thaimonth, $month);
 }
 
-function insert_zero_f_position($number, $pos) {
+function insert_zero_f_position($number, $pos)
+{
 
     $cnt = strlen($number);
     $str = $number;
